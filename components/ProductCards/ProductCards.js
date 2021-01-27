485e309e42119be455294772d6085db963553678
kellenwiltshire <kellenwiltshire@outlook.com>
2021-01-27 10:18:57 -0500

    Working on API Requests

--- components/ProductCards/ProductCards.js
+++ components/ProductCards/ProductCards.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-function ProductCards() {
-	return <div></div>;
+function ProductCards({ description, title, price, listing }) {
+	// imageFull = image.filter(listing);
+	return (
+		<div className='m-5'>
+			<div className='bg-white w-80 shadow-lg cursor-pointer rounded transform hover:scale-105 duration-300 ease-in-out'>
+				<div
+					style={{
+						clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 80%)',
+					}}
+				>
+					{/* <img src={imageFull.url_fullxfull} alt='' className='rounded-t' /> */}
+				</div>
+				<div className='p-4'>
+					<h2 className='text-2xl uppercase'>{title}</h2>
+					<p className='font-light text-gray-500 text-lg my-2'>${price}</p>
+					<p>{description}</p>
+					<a
+						href='#'
+						className='block bg-gray-300 py-2 px-2 text-gray-600 text-center rounded shadow-lg uppercase font-light mt-6 hover:bg-gray-400 hover:text-white duration-300 ease-in-out'
+					>
+						View
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProductCards;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
 import Head from 'next/Head';
 import Layout from '../components/Layout/Layout';
 import Splash from '../components/Splash/Splash';
+import ProductCards from '../components/ProductCards/ProductCards';
 
-export default function Home() {
+export default function Home({ data }) {
 	return (
 		<Layout title='The Knotty Island'>
 			<Splash />
-			<ProductCards />
+			{data.map((listing, i) => {
+				return (
+					<ProductCards
+						key={i}
+						description={listing.results[i].description}
+						title={listing.results[i].title}
+						price={listing.results[i].price}
+					/>
+				);
+			})}
 		</Layout>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const etsyAPI = 'jf8evf2p7kh6ihl16tew41k4';
+	let data = {};
+	let image = [{}];
+	try {
+		const res = await fetch(
+			`https://openapi.etsy.com/v2/shops/TheKnottyIsland/listings/active?api_key=${etsyAPI}`,
+		);
+		const data = await res.json();
+
+		// for (i = 0; i < data.results.length(); i++) {
+		// 	const listing = data.results[i].listing_id;
+		// 	const resimage = await fetch(
+		// 		`https://openapi.etsy.com/v2/listings/${listing}/images?api_key=jf8evf2p7kh6ihl16tew41k4`,
+		// 	);
+		// 	const imageLink = await resimage.json();
+		// 	image = [{ listing: { imageLink } }, ...image];
+		// }
+	} catch (error) {
+		console.log(error);
+	}
+
+	return {
+		props: { data },
+	};
 }
