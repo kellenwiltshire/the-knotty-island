bd529ed97b335e2376406e59b2c5e01c19692d32
kellenwiltshire <kellenwiltshire@outlook.com>
2021-02-02 15:27:15 -0500

    Light cleanup. Ready for beautification

--- components/Splash/Splash.js
+++ components/Splash/Splash.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 
-function Splash(props) {
+function Splash() {
 	return (
 		<div className='bg-auto bg-center bg-no-repeat w-screen justify-items-center flex flex-row place-content-center'>
-			<div className='self-center'>
-				<Image
-					src='/splash_island.jpg'
-					alt='Logo'
-					className='object-cover bg-gray-300'
-					width={1364}
-					height={303}
-				/>
-			</div>
+			<Image
+				src='/splash_island.jpg'
+				alt='Logo'
+				className='object-cover bg-gray-300'
+				width={1364}
+				height={303}
+				objectFit='cover'
+			/>
 		</div>
 	);
 }
--- pages/categories/allProducts.js
+++ pages/categories/allProducts.js
@@ -6,14 +6,16 @@ import Image from 'next/image';
 export default function allProducts() {
 	return (
 		<Layout title='The Knotty Island || Products'>
-			<div className='bg-auto bg-center bg-no-repeat w-screen justify-items-center bg-gray-300 mb-3 self-center'>
-				<Image
-					src='/logo_med.png'
-					alt='Logo'
-					height={300}
-					width={300}
-					className='self-center'
-				/>
+			<div className='w-screen bg-gray-300'>
+				<div className='flex justify-center'>
+					<Image
+						src='/logo_med.png'
+						alt='Logo'
+						height={300}
+						width={300}
+						objectFit='contain'
+					/>
+				</div>
 			</div>
 			<div className='container'>
 				<div className='flex flex-row flex-wrap justify-center'>
